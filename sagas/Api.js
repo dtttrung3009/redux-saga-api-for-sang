ffcd1cf6d3b8572ad67ad4e0751dd6e8760b893a
DangThanhTrung1999 <pumaover@gmail.com>
2019-11-03 15:07:41 +0700

    done

---
+++ sagas/Api.js
@@ -0,0 +1,18 @@
+const urlGetMovies = 'http://5db9b400eddc81001495f0df.mockapi.io/api/movies';
+
+function* getMoviesFromApi() {
+    const json = yield fetch(urlGetMovies, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: '',
+    }).then(response=>response.json());
+    const movies = yield (json);
+    // console.log(movies);
+    return movies;
+}
+export const Api = {
+    getMoviesFromApi
+};
